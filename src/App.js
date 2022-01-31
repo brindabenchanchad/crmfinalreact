3696a40fc1087f6d282596136945830c5366592f
brinda chanchad <brinda@brindas-MacBook-Pro.local>
2022-01-31 15:46:46 +0530

    added plan,Delete plan

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/plan" element={<Plan />} />
-      <Route path="plan/add" element={<AddPlan />} />
+      <Route path="/plan/add" element={<AddPlan />} />
+
     </Routes>
   );
 }
