3d863fc65f44d73e52cde54e65721a6ca64321ef
brinda chanchad <brinda@brindas-MacBook-Pro.local>
2022-01-31 09:23:07 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div>
-      <Button variant="primary">Click Me!</Button>
+      <div className='btn btn-danger'>Click Me!</div>
     </div>
   );
 }
